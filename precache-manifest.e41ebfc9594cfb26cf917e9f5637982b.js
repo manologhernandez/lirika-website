b1ed7acdf9fcb9f978a919d4821a8daff71ceec3
Manolo Hernandez <manologhernandez@yahoo.com>
2020-05-11 21:22:02 +0800

    Updates

---
+++ precache-manifest.e41ebfc9594cfb26cf917e9f5637982b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1437dafb6ad37157fbeaf4d0578d1284",
+    "url": "/lirika-website/index.html"
+  },
+  {
+    "revision": "37fbfe4ab2c0ef6698bb",
+    "url": "/lirika-website/static/js/2.f332fda8.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/lirika-website/static/js/2.f332fda8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77c84a15cfb969299f7",
+    "url": "/lirika-website/static/js/main.02b32fa4.chunk.js"
+  },
+  {
+    "revision": "2c610e5696b927827857",
+    "url": "/lirika-website/static/js/runtime-main.330e7e55.js"
+  },
+  {
+    "revision": "8681cf13f561f4e21647c32814190362",
+    "url": "/lirika-website/static/media/asset-pic-1.8681cf13.jpg"
+  },
+  {
+    "revision": "0320f4d56e14dcee35abd55a4c61f00e",
+    "url": "/lirika-website/static/media/daena.0320f4d5.jpg"
+  },
+  {
+    "revision": "1d74f4da24a5eac9ebd0648950ea961f",
+    "url": "/lirika-website/static/media/erwin.1d74f4da.jpg"
+  },
+  {
+    "revision": "fc24443fc90360282b1333d34cd739f0",
+    "url": "/lirika-website/static/media/joana.fc24443f.jpg"
+  },
+  {
+    "revision": "7e02b6abdb4478b32da7856f825d5e3f",
+    "url": "/lirika-website/static/media/lirika-logo-transparent.7e02b6ab.png"
+  },
+  {
+    "revision": "1d55f7b64b966f1cf329f510616d03a2",
+    "url": "/lirika-website/static/media/manolo.1d55f7b6.jpg"
+  },
+  {
+    "revision": "340ae797cc419db529105b611651d9b1",
+    "url": "/lirika-website/static/media/mark.340ae797.jpg"
+  },
+  {
+    "revision": "5a3961ce4cdbc042581f7dcce8da59a7",
+    "url": "/lirika-website/static/media/pam.5a3961ce.jpg"
+  },
+  {
+    "revision": "1cdfff7485864bdfd3506f3cf555c8dd",
+    "url": "/lirika-website/static/media/perf-pic-1.1cdfff74.jpg"
+  },
+  {
+    "revision": "4d2449e0b8538911d88b2415e6bb7c57",
+    "url": "/lirika-website/static/media/perf-pic-3.4d2449e0.jpg"
+  },
+  {
+    "revision": "ceaa5a37917c201cec8ae9addcb87ed1",
+    "url": "/lirika-website/static/media/sea.ceaa5a37.jpg"
+  }
+]);
